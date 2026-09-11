871cbbbda2c059d41c28b0666f0e4649bb058106
devserao <seraotourjo@gmail.com>
2026-09-12 01:21:12 +0600

    set-up the react project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className="text-5xl">Dream 11</h2>
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Responsive</button>
+    </>
+  )
+}
+
+export default App
